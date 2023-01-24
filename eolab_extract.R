510abdcfb34d46d17673a6a61c6082f32a530d98
JKfuberlin <116660831+JKfuberlin@users.noreply.github.com>
2023-01-24 13:06:12 +0100

    Update eolab_extract.R
    cleaned up and changed paths to actually work on the FORCE datacube.
    Also need to continue working on lines 74-77 to see if assignment of labels actually works, currently testing.

--- eolab_extract.R
+++ eolab_extract.R
@@ -1,4 +1,4 @@
-# library(exactextractr)
+#library(exactextractr)
 library(terra)
 library(sf)
 library(tictoc)
@@ -7,27 +7,15 @@ library(stringr)
 
 
 # epsg code needs to be 3035 for shapes
-
-# prepare file structure
-# tiles = c("TLT", "TMT", "TNT", "ULU", "UMU", "UMV", "UNA", "UNU", "UNV")
-tt = st_read('/home/eouser/shapes/target_tiles_32632.gpkg')
-tiles = tt$Tile_ID
-
+tt = sf::st_read('/home/eouser/training10m_3035.gpkg')
+tiles = sf::st_read('/home/eouser/shapes/force_grid.gpkg')
+		    
 tic()
-for (g in tiles) {
+for (g in tiles$Tile_ID)
+g = as.character(g)
 path = '/force/FORCE/C1/L2/ard'
-# path = '/codede/Sentinel-2/MSI/L2A-FORCE/' 
 s2dirs = list.dirs(path = path, recursive = T)
 s2dirs.aoi = grep(pattern = g, s2dirs, value = T)
-# s2dirs.aoi2 = stringr::str_subset(
-#   s2dirs.aoi, 
-#   pattern = "/codede/Sentinel-2/MSI/L2A-FORCE//20\\d+/0([4-9])/\\d+."
-#   )
-# s2dirs.aoi3 = stringr::str_subset(
-#   s2dirs.aoi, 
-#   pattern = "/codede/Sentinel-2/MSI/L2A-FORCE//20\\d+/10/\\d+."
-# )
-# s2dirs.aoi = c(s2dirs.aoi2, s2dirs.aoi3)
 
 raster.paths = c()
 # 20221127_LEVEL2_SEN2A_BOA.tif
@@ -53,25 +41,19 @@ for (j in raster.paths) {
 }
 }
 
-# raster.paths2 = dplyr::nth(raster.paths, 10)
-
 datacube = terra::rast(raster.paths)
 
 # rename raster names to include time
 names(datacube) = raster.paths2
 
-
 shapes = sf::st_read('/home/eouser/training10m_3035.gpkg')
-
 
 a = sf::st_bbox(datacube)
 b = as(raster::extent(a[[1]],a[[3]],a[[2]],a[[4]]), "SpatialPolygons")
 c = sf::st_as_sf(b)
 sf::st_crs(c) = 3035
-d = shapes[c,]
+d = shapes[c,] # this is the subset of shapes within the extent of the datacube raster
 print(nrow(d))
-
-# shapes = dplyr::filter(shapes, Name == paste0('32', g))
 
 print("extracting")
 tic()
@@ -82,31 +64,20 @@ shapes.extract = terra::extract(datacube, d, xy = T)
 # you might want to get the exactextractr package to work which is usually faster
 # and lets you set different functions.
 
-
-
-# shapes.extract2 = sf::st_set_geometry(shapes.extract, NULL)
 toc()
 print("extraction done")
 
-shapes.extract2 = cbind(d, shapes.extract)
-# shapes.extract2 = cbind(shapes2, shapes.extract)
-# shapes.extract2 = dplyr::left_join(shapes, shape.extract, by = "fid_2")
+# now we need to join the label on the extracted values
+# and assign an ID to each observation
+# need to convert shapes to data.frame without geometry to be able to join
+
+shapes.extract = as_tibble(shapes_extract)
+lookup = as_tibble(shapes)
+shapes.extract = tibble::rowid_to_column(shapes.extract, "ID")
+shapes.extract$species = dplyr::left_join(shapes.extract, shapes$BST1_BA_1, by=c('ID'='fid_2'))
 
 print(paste0("writing",g))
 write.csv(shapes.extract2, file = paste0('/home/eouser/csv/points',g,'.csv'))
 print(paste0(g,"written"))
-}
+
 toc()
-
-
-# shapes.extract = exactextractr::extract_extract(datacube, shapes)
-# doesn't work with terras spatrasters. need to find another way for polygon-wise weighted extractions
-
-poly.df = tibble(x = c(390000, 390050, 390050, 390000, 390000), y = c(5200050, 5200050, 5200000, 5200000, 5200050))
-poly.shape = sf::st_as_sf(poly.df, coords = c('x', 'y')) %>% st_set_crs(32632)
-datacube2 = raster::stack(datacube)
-test = raster::crop(datacube2, poly.shape)
-
-# inspect
-ULU = read.csv('/home/j/FF/csv/ULU.csv')
-names(which(colSums(is.na(ULU)) > 0))
