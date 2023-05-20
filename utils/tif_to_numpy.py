839f3fee9339499e443129a471abe005809615ff
Jonathan K <jonathan.koeltzow@fu-berlin.de>
2023-05-20 11:40:53 +0200

    added model loading and transformation to numpy, need to check if it works

--- utils/tif_to_numpy.py
+++ utils/tif_to_numpy.py
@@ -1,23 +1,11 @@
-# import tifffile as tiff
-# import numpy as np
-#
-# # Load the TIFF file
-# image_path = '/home/j/data/s2test.tif'
-# image = tiff.imread(image_path)
-#
-# # Convert the image to a NumPy array
-# image_array = np.array(image)
-#
-# print(image_array)
-# # Now you can use the image_array for prediction with your trained model
-#
-
 import os
 import glob
 import re
 import rasterio
 import geopandas as gpd
 import numpy as np
+import torch
+import joblib
 
 # Set the EPSG code
 epsg_code = 3035
@@ -60,18 +48,29 @@ for path in raster_paths:
 
 # Load raster data as a datacube
 datacube = np.stack([rasterio.open(raster_path).read() for raster_path in raster_paths], axis=-1)
+# datacube should now be a numpy array and ready for inference
 
-print(f"Loading rasters {g}")
-# Rename raster names to include time
-datacube_names = dict(zip(range(1, 11), raster_paths2))
-datacube = datacube.rename(datacube_names, axis=-1)
+# don't need this:
+# print(f"Loading rasters {g}")
+# # Rename raster names to include time
+# datacube_names = dict(zip(range(1, 11), raster_paths2))
+# datacube = datacube.rename(datacube_names, axis=-1)
+#
+# # Extract bounding box from datacube and convert it to a GeoDataFrame
+# bbox = shapes.total_bounds
+# b = gpd.GeoSeries(gpd.GeoDataFrame(geometry=[gpd.box(*bbox)]))
+# b.crs = f"EPSG:{epsg_code}"
+# shapes_extract = gpd.sjoin(shapes, b, how='inner', op='intersects')
+# print(len(shapes_extract))
+#
+# # Convert datacube raster to a matrix
+# mymat = datacube.reshape((-1, datacube.shape[-1]))
 
-# Extract bounding box from datacube and convert it to a GeoDataFrame
-bbox = shapes.total_bounds
-b = gpd.GeoSeries(gpd.GeoDataFrame(geometry=[gpd.box(*bbox)]))
-b.crs = f"EPSG:{epsg_code}"
-shapes_extract = gpd.sjoin(shapes, b, how='inner', op='intersects')
-print(len(shapes_extract))
+model = joblib.load('/my_volume/bi_lstm_demo.pkl')
+datacube = np.load('/my_volume/datacube.npy')
 
-# Convert datacube raster to a matrix
-mymat = datacube.reshape((-1, datacube.shape[-1]))
+# need to transform datacube numpy array into tensor
+data_for_prediction = datacube_tensor.reshape(-1, num_bands)
+data_for_prediction = torch.tensor(data_for_prediction)
+predictions = model.predict(data_for_prediction)
+#x_set = torch.from_numpy(datacube)
