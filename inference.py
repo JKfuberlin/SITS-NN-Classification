8cc7530aaeb65c0b3315ceaf1fdea614e15e5880
JKfuberlin <jonathan.koeltzow@fu-berlin.de>
2023-06-06 13:06:13 +0200

    WORKING STATE this version runs on EOlab and creates a map. Now need to figure out how to write it using the correct projection and origin etc

--- inference.py
+++ inference.py
@@ -16,10 +16,8 @@ device = torch.device('cpu') # assigning cpu for inference
 model_pkl = torch.load('/my_volume/bi_lstm_demo.pkl',  map_location=torch.device('cpu')) # loading the trained model
 
 def predict(input):
-    print('.')
     outputs = model_pkl(input)
     _, predicted = torch.max(outputs.data, 1)
-    print('*')
     return predicted
 
 raster_paths = []
@@ -78,12 +76,11 @@ grid_tiles = glob.glob(os.path.join('/my_volume/FORCE_tiles_subset_BW/', '*' + t
 
 minitile = grid_tiles[0]
 crop_shape = gpd.read_file(minitile)
-clipped_datacube = []
+clipped_datacube = [] # empty object for adding clipped rasters
 
 for raster in datacube:  # clip the rasters using the geometry
     print(minitile)
     crop_shape = gpd.read_file(minitile)
-    clipped_datacube = []  # empty object for adding clipped rasters
     try:
         print('yes')
         clipped_raster = raster.rio.clip(crop_shape.geometry.apply(mapping))
@@ -92,33 +89,44 @@ for raster in datacube:  # clip the rasters using the geometry
         print('no')
         break
 
-datacube_np = np.array(clipped_datacube) # this is now a clipped datacube for the first minitile
-data_for_prediction = torch.tensor(datacube_np) # int16
-data_for_prediction = data_for_prediction.to(torch.float32) # TODO: where did the sequence length go? it's 500x500x10x1
+datacube_np = np.array(clipped_datacube, ndmin = 4) # this is now a clipped datacube for the first minitile, fixing it to be 4 dimensions
+datacube_np.shape
+datacube_torch16 = torch.tensor(datacube_np) # turn the numpy array into a pytorch tensor, the result is in int16..
+datacube_torch32 = datacube_torch16.to(torch.float32) # ...so we need to transfert it to float32 so that the model can use it as input
+datacube_torch32.shape # torch.Size([320, 10, 500, 500])
 
-x = datacube_np.shape[2]  # i need the dimensions of the raster to create an empty one
-y = datacube_np.shape[3]
-# prediction = torch.zeros([x, y])  # create empty tensor where results will be written
-# data_for_prediction = data_for_prediction.permute(2, 3, 0, 1)  # rearrange data
-yv
-data = data_for_prediction.view(10, 500, -1) # this turns the data into a 3D tensor of rowsxcolumnsxbands
-data_for_prediction = data.permute(2, 1, 0) # reorders
+data_for_prediction = datacube_torch32.permute(2, 3, 0, 1)  # rearrange data
+data_for_prediction.shape #torch.Size([500, 500, 320, 10])
 
-ct1 = datetime.datetime.now()
-print("current time:-", ct1)
-predictions = predict(data_for_prediction) # TODO: what does this represent? a row? a column?
-ct = datetime.datetime.now()
-print("current time:-", ct)
-print("start time:-", ct1)
-print('done')
+# data = data_for_prediction.reshape(320, 10, -1) # torch.Size([320, 10, 250000])
 
-predictions = torch.tensor(predictions)  # Convert the list to an array
-map = predictions.numpy()
+# data = data_for_prediction2.view(320, 10, -1)
+# data = data_for_prediction2.view(10, 500, -1) # this turns the data into a 3D tensor of rowsxcolumnsxbands
+# data_for_prediction_3d = data.permute(2, 1, 0) # reorders
+
+# x = data_for_prediction_3d.shape[2]  # i need the dimensions of the raster to create an empty one
+# y = data_for_prediction_3d.shape[3]
+x = 500
+y = 500
+
+result = torch.zeros([x, y])  # create empty tensor where results will be written
+
+for row in range(x):
+    predictions = predict(data_for_prediction[row])
+    print(row)
+    result[row] = predictions
+
+result = torch.tensor(result)  # Convert the list to an array
+map = result.numpy()
+
+with rasterio.open(os.path.join('/my_volume/', 'example.tif'), 'w') as dst:
+    dst.write_band(1, map.astype(rasterio.float32))
 
 
-for i in range(x):
-    input = data_for_prediction[i]
-    outputs = model_pkl(input)
-    _, predicted = torch.max(outputs.data, 1)
-    prediction[i] = predicted
-    print(i)
+# i rewrote this into a function
+# for i in range(x):
+#     input = data_for_prediction[i]
+#     outputs = model_pkl(input)
+#     _, predicted = torch.max(outputs.data, 1)
+#     prediction[i] = predicted
+#     print(i)
