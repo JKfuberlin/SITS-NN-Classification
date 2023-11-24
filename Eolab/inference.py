0a07d1c68363bfe868f84c45a1ddeb58e5f3f00c
Florian Katerndahl <florian@katerndahl.com>
2023-11-24 18:10:16 +0000

    first working refactor of lstm inference

--- Eolab/inference.py
+++ Eolab/inference.py
@@ -1,144 +1,135 @@
-import os # for general operating system functionality
-import glob # for retrieving files using strings/regex
-import re # for regex
-import rasterio # for reading rasters
-import geopandas as gpd # for reading shapefiles
+# TODO use argparse and maybe even outsource it to its own class
+# TODO convert to stand-alone script which can be run via the command line
+# TODO needless data type conversion when saving output image?
+# TODO package models into a library so that importing is easier
+# TODO fix GPU inference: https://stackoverflow.com/questions/71278607/pytorch-expected-all-tensors-on-same-device
+# TODO try inference with 500 pixel by 500 pixel size
+import sys
+from pathlib import Path
+from re import search
+from typing import List, Any, Union, Dict
+
 import numpy as np
-import datetime # for benchmarking
-import torch # for loading the model and actual inference
-import rioxarray as rxr # for raster clipping
-import multiprocessing # for parallelization
-from shapely.geometry import mapping # for clipping
-from rasterio.transform import from_origin # for assigning an origin to the created map
+import rasterio
+import rioxarray as rxr
+import torch
+import xarray
+import geopandas as gpd
+from shapely.geometry import mapping
+import logging
+from time import time
 
-# tiles = np.loadtxt('/my_volume/BW_tiles.txt', dtype=str) # this file contains the XY tile names of my AOI in the same format as FORCE
+import models
 
-device = torch.device('cpu') # assigning cpu for inference
-model_pkl = torch.load('/my_volume/bi_lstm_demo.pkl',  map_location=torch.device('cpu')) # loading the trained model
+logging.basicConfig(level=logging.INFO, filename="/home/eouser/git-repos/futureforest/Eolab/runtime.txt")
 
-def predict(input):
-    outputs = model_pkl(input)
+INPUT_TILE_PATH: str = "/home/eouser/git-repos/futureforest/Eolab/tiles.txt"
+INPUT_WEIGHTS_PATH: str = "/home/eouser/git-repos/futureforest/Eolab/LSTM_26.pkl"
+S2_BASE_DIR: Path = Path("/force/FORCE/C1/L2/ard/")
+OUTPUT_BASE_DIR: Path = Path("/home/eouser/git-repos/futureforest/outputs/data")
+MASK_BASE_DIR: Path = Path("/home/eouser/git-repos/futureforest/FORCE_minibatches")
+LANDSHUT: str = "X0067_Y0058"
+MINITILE: int = 31
+CHUNK_SIZE = 1000  # GeoPackages are 500 pixel by 500 pixel
+NEWEST_CUTOFF: int = 20230302
+
+with open(INPUT_TILE_PATH, "rt") as f:
+    FORCE_tiles: List[str] = [tile.replace("\n", "") for tile in f.readlines()]
+
+lstm: torch.nn.LSTM = torch.load(INPUT_WEIGHTS_PATH, map_location=torch.device('cpu'))
+
+template_metadata: Dict[str, Any] = {
+    'driver': 'GTiff',
+    'width': None,
+    'height': None,
+    'count': 1,
+    'dtype': rasterio.float32,
+    'crs': 'EPSG:3035',
+    'transform': None
+}
+
+
+def predict(model, data: torch.tensor) -> Any:
+    """
+    Apply previously trained LSTM to new data
+    :param model: previously trained model
+    :param torch.tensor data: new input data
+    :return Any: Array of predictions
+    """
+    # TODO https://pytorch.org/docs/stable/notes/cpu_threading_torchscript_inference.html suggests that
+    #      multi-threading is possible and I observe multi-CPU usage.
+    #      Is multi-threading on by default? Documentation suggests no, but I'm not sure.
+    #      torch.__config__.parallel_info() suggests, that multi-threading is active!
+    outputs = model(data)
     _, predicted = torch.max(outputs.data, 1)
     return predicted
 
-raster_paths = []
 
-tile ="X0067_Y0058" # Landshut
+for tile in FORCE_tiles:
+    start: float = time()
+    #logging.info(f"Loading mask for FORCE tile {MASK_BASE_DIR / Path(LANDSHUT) / (str(MINITILE) + '.gpkg')}")
+    #minitile = gpd.read_file(MASK_BASE_DIR / Path(LANDSHUT) / (str(MINITILE) + ".gpkg"))
 
-for tile in tiles:
-    s2dir = glob.glob(('/force/FORCE/C1/L2/ard/'+tile+'/'), recursive=True) # Initialize path to Sentinel-2 time series
-    raster_paths = [] # create object to be filled with rasters to be stacked
-    tifs = glob.glob(os.path.join(s2dir[0], "*SEN*BOA.tif")) # i want to ignore the landsat files and stack only sentinel 2 bottom of atmosphere observations
-    raster_paths.extend(tifs) # write the ones i need into my object to stack from
-    years = [int(re.search(r"\d{8}", raster_path).group(0)) for raster_path in raster_paths] # i need to extract the date from each file path...
-    raster_paths = [raster_path for raster_path, year in zip(raster_paths, years) if year <= 20230302] # ... to cut off at last image 20230302 as i trained my model with this sequence length, this might change in the future with transformers
-    datacube = [rxr.open_rasterio(raster_path) for raster_path in raster_paths]  # i user rxr because the clip function from rasterio sucks
-    # the datacube is too large for the RAM of my contingent so i need to subset using the 5x5km tiles
-    grid_tiles = glob.glob(os.path.join('/my_volume/FORCE_tiles_subset_BW/', '*' + tile + '*.gpkg'))
-    for minitile in grid_tiles: # now i iterate over the small grids
-        crop_shape = gpd.read_file(minitile) # open the corresponding geometry
-        clipped_datacube = [] # empty object for adding clipped rasters
-        for raster in datacube: # clip the rasters using the geometry
-            clipped_raster = raster.rio.clip(crop_shape.geometry.apply(mapping))
-            clipped_datacube.append(clipped_raster)
-    x=clipped_datacube.shape[2] # i need the dimensions of the raster to create an empty one
-    y=clipped_datacube.shape[3]
-    prediction = torch.zeros([x,y]) # create empty tensor where results will be written
-    data_for_prediction = data_for_prediction.permute(2, 3, 0, 1) # rearrange data
-    num_cores = 15 # Define the number of cores to use
-    pool = multiprocessing.Pool(processes=num_cores) # Create a multiprocessing pool
-    predictions = pool.map(predict, data_for_prediction) # Iterate over the range in parallel and store the results in a list
-    # Close the pool to release resources
-    pool.close()
-    pool.join()
-    predictions = torch.tensor(predictions) # Convert the list to an array
-    map = prediction.numpy()
-    with rasterio.open(raster_paths[0]) as src:  # i can use any image from the stack cuz they all the same
-        metadata = src.meta
-        # Update the number of bands in the metadata to match the modified image
-        metadata['count'] = 1
-    # writing
-    with rasterio.open('/my_volume/map_BILSTM_'+tile+'_'+minitile, 'w',**metadata) as dst:  # open write connection to file using the metadata
-        dst.write(map, 1)
+    logging.info(f"Processing FORCE tile {tile}")
+    s2_tile_dir: Path = S2_BASE_DIR / tile
+    tile_paths: List[str] = [str(p) for p in s2_tile_dir.glob("*SEN*BOA.tif")]
+    cube_inputs: List[str] = [
+        tile_path for tile_path in tile_paths if int(search(r"\d{8}", tile_path).group(0)) <= NEWEST_CUTOFF
+    ]
 
+    # TODO there may exist an easier solution than opening an image from my tile stack
+    with rasterio.open(cube_inputs[0]) as f:
+        metadata = f.meta
+        metadata["count"] = 1
+        metadata["dtype"] = rasterio.float32
 
-# testing for a single tile
+    assert metadata['height'] % 2 == 0 and metadata['width'] % 2 == 0
 
-tile ="X0067_Y0058" # Landshut
-s2dir = glob.glob(('/force/FORCE/C1/L2/ard/' + tile + '/'), recursive=True)  # Initialize path to Sentinel-2 time series
-raster_paths = []  # create object to be filled with rasters to be stacked
-tifs = glob.glob(os.path.join(s2dir[0], "*SEN*BOA.tif"))  # i want to ignore the landsat files and stack only sentinel 2 bottom of atmosphere observations
-raster_paths.extend(tifs)  # write the ones i need into my object to stack from
-years = [int(re.search(r"\d{8}", raster_path).group(0)) for raster_path in raster_paths]  # i need to extract the date from each file path...
-raster_paths = [raster_path for raster_path, year in zip(raster_paths, years) if year <= 20230302]  # ... to cut off at last image 20230302 as i trained my model with this sequence length, this might change in the future with transformers
-datacube = [rxr.open_rasterio(raster_path) for raster_path in raster_paths]  # i user rxr because the clip function from rasterio sucks
-# the datacube is too large for the RAM of my contingent so i need to subset using the 5x5km tiles
-# grid_tiles = glob.glob(os.path.join('/my_volume/FORCE_tiles_subset_BW/', '*' + tile + '*.gpkg'))
-grid_tiles = '/my_volume/31.gpkg'
-# checking workflow:
+    tile_rows: int = metadata["height"]  # s2_cube_prediction.shape[2]
+    tile_cols: int = metadata["width"]  # s2_cube_prediction.shape[3]
+    output_torch: torch.tensor = torch.zeros([tile_rows, tile_cols])
 
-minitile = grid_tiles
-crop_shape = gpd.read_file(minitile)
-clipped_datacube = [] # empty object for adding clipped rasters
+    for row in range(0, tile_rows, CHUNK_SIZE):
+        for col in range(0, tile_cols, CHUNK_SIZE):
+            start_chunked: float = time()
+            logging.info(f"Creating chunked data cube")
+            s2_cube: Union[xarray.Dataset, xarray.DataArray, list[xarray.Dataset]] = []
+            for cube_input in cube_inputs:
+                layer: Union[xarray.Dataset, xarray.DataArray] = rxr.open_rasterio(cube_input)
+                clipped_layer = layer.isel(x=slice(row, row + CHUNK_SIZE), y=slice(col, col + CHUNK_SIZE))
+                s2_cube.append(clipped_layer)
 
-iti = 1
-total = str(len(datacube))
-for raster in datacube:  # clip the rasters using the geometry
-    crop_shape = gpd.read_file(minitile)
-    print('cropping ' + str(iti) + ' out of ' + total)
-    iti = iti + 1
-    try:
-        clipped_raster = raster.rio.clip(crop_shape.geometry.apply(mapping))
-        clipped_datacube.append(clipped_raster)
-    except:
-        print('not working')
-        break
+            logging.info(f"Converting chunked data cube to numpy array")
+            s2_cube_np: np.ndarray = np.array(s2_cube, ndmin=4)
+            logging.info(f"Converting chunked numpy array to torch tensor")
+            s2_cube_torch: torch.tensor = torch.tensor(s2_cube_np, dtype=torch.float32)
+            logging.info(f"Permuting torch tensor")
+            s2_cube_prediction: torch.tensor = s2_cube_torch.permute(2, 3, 0, 1)
 
-datacube_np = np.array(clipped_datacube, ndmin = 4) # this is now a clipped datacube for the first minitile, fixing it to be 4 dimensions
-datacube_np.shape
-datacube_torch16 = torch.tensor(datacube_np) # turn the numpy array into a pytorch tensor, the result is in int16..
-datacube_torch32 = datacube_torch16.to(torch.float32) # ...so we need to transfert it to float32 so that the model can use it as input
-datacube_torch32.shape # torch.Size([320, 10, 500, 500])
+            # TODO why not use smaller image tiles as input? is the lstm fixed to a certain input length?
+            #  alternatively: below, the prediction is done row-wise per tile. The clipping should not matter in that
+            #  case!
+            for chunk_rows in range(0, CHUNK_SIZE):
+                start_row: float = time()
+                output_torch[row + chunk_rows, col:col + CHUNK_SIZE] = predict(lstm, s2_cube_prediction[chunk_rows])
+                logging.info(f"Processed row {chunk_rows}/{CHUNK_SIZE - 1} of "
+                             f"row-wise chunk: {row}:{row + CHUNK_SIZE - 1}, column-wise chunk: {col}:{col + CHUNK_SIZE - 1} "
+                             f"({tile = }) in {time() - start_chunked:.2f} seconds")
 
-data_for_prediction = datacube_torch32.permute(2, 3, 0, 1)  # rearrange data
-data_for_prediction.shape #torch.Size([500, 500, 320, 10])
+            logging.info(f"Processed chunk in {time() - start_chunked} seconds")
 
-# data = data_for_prediction.reshape(320, 10, -1) # torch.Size([320, 10, 250000])
+    output_numpy: np.array = output_torch.numpy()
 
-# data = data_for_prediction2.view(320, 10, -1)
-# data = data_for_prediction2.view(10, 500, -1) # this turns the data into a 3D tensor of rowsxcolumnsxbands
-# data_for_prediction_3d = data.permute(2, 1, 0) # reorders
+    # TODO save as int not float
+    with rasterio.open(OUTPUT_BASE_DIR / (tile + ".tif"), 'w', **metadata) as dst:
+        dst.write_band(1, output_numpy.astype(rasterio.float32))
 
-# x = data_for_prediction_3d.shape[2]  # i need the dimensions of the raster to create an empty one
-# y = data_for_prediction_3d.shape[3]
-x = 500
-y = 500
+    logging.info(f"Processed tile {tile} in {time() - start} seconds")
 
-result = torch.zeros([x, y])  # create empty tensor where results will be written
+    del metadata
+    del s2_cube
+    del s2_cube_torch
+    del s2_cube_prediction
+    del output_torch
+    del output_numpy
 
-for row in range(x):
-    predictions = predict(data_for_prediction[row])
-    print(row)
-    result[row] = predictions
-
-result = torch.tensor(result)  # Convert the list to an array
-map = result.numpy()
-
-crop_geometry = crop_shape.geometry.iloc[0]
-crop_bounds = crop_geometry.bounds
-origin = (crop_bounds[0], crop_bounds[3])  # Extract the origin from the bounds
-
-
-# construct metadata from minitile and matrix
-metadata = {
-    'driver': 'GTiff',
-    'width': map.shape[1],
-    'height': map.shape[0],
-    'count': 1,  # Number of bands
-    'dtype': map.dtype,
-    'crs': 'EPSG:3035',  # Set the CRS (coordinate reference system) code as needed
-    'transform': from_origin(origin[0], origin[1], 10, 10)  # Set the origin and pixel size (assumes each pixel is 1 unit)
-}
-
-with rasterio.open(os.path.join('/my_volume/', 'landshut_example2.tif'), 'w', **metadata) as dst:
-    dst.write_band(1, map.astype(rasterio.float32))
